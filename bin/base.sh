130274a69ec982c5f11adc6b9f3200424e2b4bee
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-08-05 11:35:44 +0200

    Added TEMP_DIR constant. Added dictionary of releases and their Download URIs. Added script to automatically fetch a release

--- bin/base.sh
+++ bin/base.sh
@@ -8,6 +8,7 @@ SCRIPTS_DIR="$PHPENV_ROOT/bin"
 PACKAGE_DIR="$PHPENV_ROOT/packages"
 SOURCE_DIR="$PHPENV_ROOT/source"
 TARGET_DIR="$PHPENV_ROOT/versions"
+TEMP_DIR="$PHPENV_ROOT/tmp"
 
 if [ ! -d $PACKAGE_DIR ]; then
     mkdir $PACKAGE_DIR
@@ -19,4 +20,8 @@ fi
 
 if [ ! -d $TARGET_DIR ]; then
     mkdir $TARGET_DIR
+fi
+
+if [ ! -d $TEMP_DIR ]; then
+    mkdir $TEMP_DIR
 fi
---
+++ bin/fetch-release.sh
@@ -0,0 +1,51 @@
+#/bin/bash
+
+source "$(dirname $0)/base.sh"
+
+function display_usage {
+    echo "Usage: $0 [release]"
+    echo
+    echo "Example: $0 php-5.4.0alpha3"
+}
+
+if [ -z $1 ]; then
+    display_usage
+    exit -1
+fi
+
+release=$1
+
+function download {
+    local rel=$1
+    local uri=$2
+
+    if [ -f $PACKAGE_DIR/$rel.tar.bz2 ]; then
+        echo "Tarball for Release $rel already fetched in $PACKAGE_DIR/$rel.tar.bz2."
+        exit 1
+    fi
+
+    echo "Fetching Release Tarball for $rel from $uri"
+
+    wget -qP $TEMP_DIR $uri
+
+    if [ 0 -ne $? ]; then
+        echo
+        echo "Error while fetching $rel from $uri"
+        exit 1
+    fi
+
+    cp "$TEMP_DIR/$rel.tar.bz2" "$PACKAGE_DIR/"
+    rm "$TEMP_DIR/$rel.tar.bz2"
+
+    echo "Done."
+    exit 0
+}
+
+while read name uri
+do
+    if [ "$name" = "$release" ]; then
+        download $release $uri
+    fi
+done <"$PHPENV_ROOT/releases"
+
+echo "Release $release not found."
