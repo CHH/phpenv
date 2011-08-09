04860afe2687ca58fac09b4bfe4ad3bfe430d343
Christoph Hochstrasser <christoph.hochstrasser@gmail.com>
2011-08-09 15:17:24 +0200

    Added '*' to the currently enabled version

--- scripts/list-versions.sh
+++ scripts/list-versions.sh
@@ -3,4 +3,19 @@
 # Load path constants
 source "$PHPENV_SCRIPTS_DIR/base.sh"
 
-ls -1 --ignore local $TARGET_DIR
+packages=$(ls -1 --ignore local $TARGET_DIR)
+
+if [ -f "$PHPENV_ROOT/enabled" ]; then
+    enabled=$(cat "$PHPENV_ROOT/enabled")
+else
+    enabled=
+fi
+
+for package in $packages
+do
+    if [ -n "$enabled" ] && [ "$enabled" = "$package" ]; then
+        echo "* $package"
+    else
+        echo "  $package"
+    fi
+done
